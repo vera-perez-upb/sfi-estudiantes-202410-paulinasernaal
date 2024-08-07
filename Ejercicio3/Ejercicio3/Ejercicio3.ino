97b2d8bfec1b842db733c7968c4f688a6f25b753
Maria Paulina <mariapaulinasernaalzate@gmail.com>
2024-08-07 09:12:15 -0500

    Docs: 100

---
+++ Ejercicio3/Ejercicio3/Ejercicio3.ino
@@ -0,0 +1,16 @@
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  static uint32_t previousTime = 0;
+  static bool ledState = true;
+
+  uint32_t currentTime = millis();
+
+  if( (currentTime - previousTime) > 100){
+    previousTime = currentTime;
+    ledState = !ledState;
+    digitalWrite(LED_BUILTIN, ledState);
+  }
+}
